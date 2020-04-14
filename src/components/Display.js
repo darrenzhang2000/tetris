faf79dbe5f0af505222e14d665c596dc11f41c61
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-14 03:29:52 -0400

    added styled display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { StyledDisplay } from './styles/StyledDisplay'
 
 const Display = ({ gameOver, text }) => <div>
-    <div> { text } </div>
+    <StyledDisplay> { text } </StyledDisplay>
 </div>
 
 export default Display
