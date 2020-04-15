4ebb8af161ae57cda01c02cc2ea1ecec2f80053f
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-15 01:23:41 -0400

    added hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useState } from "react"
 
-import { createStage } from '../gameHelpers';
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+//Styled Components
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris"
+
+//Custom hooks
+import { usePlayer } from "../hooks/usePlayer"
+import { useStage } from "../hooks/useStage"
 
 // Components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage"
+import Display from "./Display"
+import StartButton from "./StartButton"
 
 const Tetris = () => {
-  return (
-    <StyledTetrisWrapper>
-      <StyledTetris>
-        <Stage stage={createStage()} />
-        <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
-          <StartButton />
-        </aside>
-      </StyledTetris>
-    </StyledTetrisWrapper>
-  );
-};
+    const [dropTime, setDropTime] = useState(null)
+    const [gameOver, setGameOver] = useState(false)
 
-export default Tetris;
+    const [player] = usePlayer()
+    const [stage, setStage] = useStage(player)
+
+    return (
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={stage} />
+                <aside>
+                    { gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over"/>
+                    ): (
+                    <div>
+                        <Display text="Score" />
+                        <Display text="Rows" />
+                        <Display text="Level" />
+                    </div>
+                    )}
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
+    )
+}
+
+export default Tetris
