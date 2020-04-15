1c496be8543c3d8c430c69d90a164f7554226583
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-15 19:16:37 -0400

    fixed bugs

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,4 +1,3 @@
-  
 import React from 'react';
 import { StyledStartButton } from './styles/StyledStartButton';
 
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,3 +1,4 @@
+  
 import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
