ad365f5de5db3706d82802a8eb2014ad8ecddb8b
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-15 18:11:23 -0400

    tetraminos start at the middle

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,76 +1,79 @@
-import React, { useState } from "react"
+  
+import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers'
+import { createStage } from '../gameHelpers';
 
-//Styled Components
-import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris"
+// Styled Components
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
-//Custom hooks
-import { usePlayer } from "../hooks/usePlayer"
-import { useStage } from "../hooks/useStage"
+// Custom Hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
 
 // Components
-import Stage from "./Stage"
-import Display from "./Display"
-import StartButton from "./StartButton"
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
 
 const Tetris = () => {
-    const [dropTime, setDropTime] = useState(null)
-    const [gameOver, setGameOver] = useState(false)
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
 
-    const [player, updatePlayerPos, resetPlayer] = usePlayer()
-    const [stage, setStage] = useStage(player)
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player);
 
+  console.log('re-render');
 
-    const movePlayer = dir => {
-        updatePlayerPos({ x: dir, y: 0 })
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  }
+
+  const startGame = () => {
+    console.log("test")
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
     }
+  }
 
-    const startGame = () => {
-        // Reset everything
-        setStage(createStage())
-        resetPlayer()
-    }
+  return (
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+      <StyledTetris>
+        <Stage stage={stage} />
+        <aside>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
+          <StartButton callback={startGame} />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+};
 
-    const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false })
-    }
-
-    const dropPlayer = () => {
-        drop()
-    }
-
-    const move = ({ keyCode }) => {
-        if(!gameOver){
-            if (keyCode === 37){
-                movePlayer(-1)
-            } else if (keyCode === 39){
-                movePlayer(1)
-            } else if (keyCode === 40){
-                dropPlayer()
-            }
-        }
-    }
-
-    return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
-            <StyledTetris>
-                <Stage stage={stage} />
-                <aside>
-                    { gameOver ? (
-                        <Display gameOver={gameOver} text="Game Over"/>
-                    ): (
-                    <div>
-                        <Display text="Score" />
-                        <Display text="Rows" />
-                        <Display text="Level" />
-                    </div>
-                    )}
-                    <StartButton onClick={startGame}/>
-                </aside>
-            </StyledTetris>
-        </StyledTetrisWrapper>
-    )
-}
-
-export default Tetris
+export default Tetris;
